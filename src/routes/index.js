b0ee163f6a2e839e0578e9325a97ba02dc73b6dc
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-01-08 16:06:20 -0600

    Inyecta los middlewares y realiza la configuración de todas las rutas

---
+++ src/routes/index.js
@@ -0,0 +1,33 @@
+//Inyecta los middlewares y realiza la configuración de todas las rutas.
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')//Brechas de seguridad. (middleware).
+const compression = require('compression')//Comprime las peticiones Http.
+require('express-async-errors')//Captura en un middleware las excepciones asíncronas.
+const { NotFoundMiddleware, ErrorMiddleware } = require("../middlewares")
+
+module.exports= function({HomeRoutes}) {
+  const router = express.Router()
+  const apiRoutes = express.Router()
+
+//Default-middlewares de express que se ejecutan antes de las rutas.
+  apiRoutes
+  .use(express.json()) //Convierte nuestro body a JSON. Equivalente a Body-parser.
+  .use(cors())
+  .use(helmet())
+  .use(compression());
+
+  apiRoutes.use("/home", HomeRoutes);
+
+//Versión que tendrán todos los enpoints.
+  router.use("/v1/api", apiRoutes);
+
+//Captura de errores y status.
+  router.use(NotFoundMiddleware);
+  router.use(ErrorMiddleware);
+
+
+  return router;
+}
+
+//cors(), puede recibir algunas especificaciones. (investigar).
