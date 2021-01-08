c9fd955743267fa2134dff4e0e3bb705f2a11e41
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-01-08 16:19:23 -0600

    Almacenará las rutas que existan de home

---
+++ src/routes/home.routes.js
@@ -0,0 +1,11 @@
+const { Router } = require('express')
+
+//Awilix porporciona el HomeController.
+module.exports = function({ HomeController }){
+  const router = Router();
+
+//Gracias al bind que configuramos antes, el scoup de HomeController se mantiene y podemos acceder al 'services'.
+  router.get("/", HomeController.index)
+
+  return router
+};
