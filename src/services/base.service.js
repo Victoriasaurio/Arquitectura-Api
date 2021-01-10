1d2d17bd68c4b1467572c62c28915c20fc739c18
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-01-10 13:33:29 -0600

    Contiene el CRUD base de los servicios

---
+++ src/services/base.service.js
@@ -0,0 +1,56 @@
+class BaseService {
+  constructor(repository) {
+    this.repository = repository
+  }
+
+  async get(id) {
+    if(!id) {
+      const error = new Error()
+      error.status = 400
+      error.message = "ID must be sent"
+      throw error; //Este error lo atrapa el error-middleware.
+    }
+
+    const currentEntity = await this.repository.get(id)
+
+    if(!currentEntity) {
+      const error =  new Error()
+      error.status = 404
+      error.message = "Entity does not found"
+      throw error;
+    }
+
+    return currentEntity; //Retorna la Entidad
+  }
+  async getAll() {
+    return await this.repository.getAll()
+  }
+
+  async create(entity) {
+    return await this.repository.create(entity)
+  }
+
+  async update(id, entity) {
+    if(!id) {
+      const error = new Error()
+      error.status = 400
+      error.message = "Id must be sent"
+      throw error;
+    }
+
+    return await this.repository.update(id, entity)
+  }
+
+  async delete(id) {
+    if(!id) {
+      const error = new Error()
+      error.status = 400
+      error.message = "Id must be sent"
+      throw error;
+    }
+
+    return await this.repository.delete(id)
+  }
+}
+
+module.exports = BaseService;
