977c20d1b91ae0228aff9c1c01a2ed189a799071
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-01-10 16:07:02 -0600

    Contiene las rutas que utiliza el controlador de idea

---
+++ src/routes/idea.routes.js
@@ -0,0 +1,19 @@
+const { Router } = require('express')
+
+//Awilix proporciona el HomeController.
+module.exports = function({ IdeaController }){
+  const router = Router();
+
+//'index' - Indica el método que devuelve al usuario.
+  router.get("/:ideaId", IdeaController.get)
+  router.get("", IdeaController.getAll)
+  router.get("/:ideaId/all", IdeaController.getUserIdeas)
+  router.post("", IdeaController.create)
+  router.patch("/:ideaId", IdeaController.update)
+  router.delete("/:ideaId", IdeaController.delete)
+  router.post(":ideaId/upvote", IdeaController.upvoteIdeas)
+  router.post(":ideaId/downvote", IdeaController.downvoteIdeas)
+
+
+  return router
+};
