a29fe55d164c134e60de45a89b6a567c9fa18e82
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-01-30 12:22:20 -0600

    CacheMiddleware - Utilización de ese middleware en la ruta user, reduciendo así el tiempo de petición GET

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,19 @@
 const { Router } = require('express')
-const { AuthMiddleware, ParseIntMiddleware } = require("../middlewares")
+const {
+  AuthMiddleware,
+  ParseIntMiddleware,
+  CacheMiddleware
+} = require("../middlewares")
+const { CACHE_TIME } = require("../helpers")
 
 //Awilix proporciona el UserController.
 module.exports = function({ UserController }){
   const router = Router();
 
   router.get("/:userId", UserController.get)
-  router.get("", [AuthMiddleware, ParseIntMiddleware],UserController.getAll) //Protege la ruta con un token.
+  router.get("",
+    [AuthMiddleware, ParseIntMiddleware, CacheMiddleware(CACHE_TIME.ONE_HOUR)], // Se cacha por una hora
+    UserController.getAll) //Protege la ruta con un token.
   router.patch("/:userId", UserController.update)
   router.delete("/:userId", UserController.delete)
 
