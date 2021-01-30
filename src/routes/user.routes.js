d15788cc216077c49e291a1587fc23bec2def78b
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-01-30 11:30:25 -0600

    ParseInt - Se agrega el middleware ParseInt a la ruta de user que permitirá realizar la respectiva paginación

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,12 @@
 const { Router } = require('express')
-const { AuthMiddleware } = require("../middlewares")
+const { AuthMiddleware, ParseIntMiddleware } = require("../middlewares")
 
 //Awilix proporciona el UserController.
 module.exports = function({ UserController }){
   const router = Router();
 
   router.get("/:userId", UserController.get)
-  router.get("", [AuthMiddleware],UserController.getAll) //Protege la ruta con un token.
+  router.get("", [AuthMiddleware, ParseIntMiddleware],UserController.getAll) //Protege la ruta con un token.
   router.patch("/:userId", UserController.update)
   router.delete("/:userId", UserController.delete)
 
