0af277a1daa36afda93d9c3ddcdd8b8fe440aae3
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-02-01 14:16:19 -0600

    Middleware - Give routes a AuthMiddleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -11,11 +11,11 @@ module.exports = function({ UserController }){
   const router = Router();
 
   router.get("/:userId", UserController.get)
-  router.get("",
-    [AuthMiddleware, ParseIntMiddleware, CacheMiddleware(CACHE_TIME.ONE_HOUR)], // Se cacha por una hora
+  router.get("", // [ ParseIntMiddleware, CacheMiddleware(CACHE_TIME.ONE_HOUR)] //Por practica de SWAGGER lo comentamos.
+    [ ParseIntMiddleware], // Se cacha por una hora
     UserController.getAll) //Protege la ruta con un token.
-  router.patch("/:userId", UserController.update)
-  router.delete("/:userId", UserController.delete)
+  router.patch("/:userId", AuthMiddleware, UserController.update)
+  router.delete("/:userId", AuthMiddleware, UserController.delete)
 
   return router
 };
